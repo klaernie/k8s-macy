0f75976a6edbc33afa1b90ffbaf500d89b9e2f9d
Andre Klärner <kandre@ak-online.be>
2025-12-09 15:11:58 +0100

    matrix: remove remnants of first setup scripts

--- matrix/gc-bridge/003_generate_cm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl -n matrix create cm gc-bridge \
-	--from-file=config.yaml=configgen/config.yaml \
-	--from-file=registration.yaml=configgen/registration.yaml \
-	--dry-run=client \
-	-o yaml > 003_configmap-temp.yaml
-
--- matrix/insta-bridge/002_create_insta-dbuser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-source ../000_pwgen.sh
-
-POD_NAME=$(kubectl -n matrix get pod -l k8s-app=postgres -o name)
-
-PGSQLPW="$(pwgen)"
-
-echo -e "$PGSQLPW\n$PGSQLPW\n" | kubectl -n matrix exec -it $POD_NAME -- /usr/bin/createuser --pwprompt instabridge -U matrix
-kubectl -n matrix exec -it $POD_NAME -- /usr/bin/createdb --encoding=UTF8 --locale=C --template=template0 --owner=instabridge instabridge -U matrix
