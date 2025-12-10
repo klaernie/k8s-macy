1ef042ec96306183ddbd204200aaca94f11e9c1e
Andre Klärner <kandre@ak-online.be>
2025-12-10 14:24:26 +0100

    matrix: migrate to cnpg database

--- matrix/copy-and-merge-config.sh
+++ matrix/copy-and-merge-config.sh
@@ -9,3 +9,7 @@ cp /cm/registration.yaml /data/
 SECRETS_FILE=
 test -f /cm/secrets.yaml && SECRETS_FILE=/cm/secrets.yaml
 yq eval-all '. as $item ireduce ({}; . * $item )' /cm/config.yaml $SECRETS_FILE >/data/config.yaml
+
+if [ -n "$DB_URI" ]; then
+	yq -i '.appservice.database.uri = strenv(DB_URI)' /data/config.yaml
+fi
