756aceadb957d6e3aa40ff0a8856691684dfb568
Andre Klärner <kandre@ak-online.be>
2025-12-10 15:36:08 +0100

    matrix: simplify configgen scripts and set podman as engine - the volmount does not work with remote docker

--- matrix/disco-bridge/configgen.sh
+++ matrix/disco-bridge/configgen.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -x
 
-bridge=discord
-
 install -d $(pwd)/configgen
-docker run --env UID=$(id -u) --env GID=$(id -g) --rm -v `pwd`/configgen:/data:z dock.mau.dev/mautrix/$bridge:latest
+podman run --env UID=$(id -u) --env GID=$(id -g) --rm -v `pwd`/configgen:/data:z dock.mau.dev/mautrix/discord:latest
--- matrix/gc-bridge/configgen.sh
+++ matrix/gc-bridge/configgen.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -x
 
-bridge=googlechat
-
 install -d $(pwd)/configgen
-docker run --rm -v `pwd`/configgen:/data:z dock.mau.dev/mautrix/$bridge:latest
+podman run --rm -v `pwd`/configgen:/data:z dock.mau.dev/mautrix/googlechat:latest
