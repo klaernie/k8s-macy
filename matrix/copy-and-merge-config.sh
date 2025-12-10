2deab3614eccdb2509f4f1751f211f8d6c6c033e
Andre Klärner <kandre@ak-online.be>
2025-12-10 15:32:03 +0100

    matrix: rotate wa-bridge appservice token and double puppeting token due to leaking it
    Also use this change to centralize the double puppeting token into the
    matrix-homeserver secret instead of saving a bunch of copies.

--- matrix/copy-and-merge-config.sh
+++ matrix/copy-and-merge-config.sh
@@ -13,3 +13,6 @@ yq eval-all '. as $item ireduce ({}; . * $item )' /cm/config.yaml $SECRETS_FILE 
 if [ -n "$DB_URI" ]; then
 	yq -i '.appservice.database.uri = strenv(DB_URI)' /data/config.yaml
 fi
+if [ -n "$PUPPETTOKEN" ]; then
+	yq -i '.bridge.login_shared_secret_map."ak-online.be" = strenv(PUPPETTOKEN)' /data/config.yaml
+fi
